a3e323daa40a9308e24244070f1ae0cddbac77b7
kensuke1003 <kensukeseven1003@gmail.com>
2021-02-21 01:11:12 +0000

    1

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,17 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/sign_up'  
+  end
+
+
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
