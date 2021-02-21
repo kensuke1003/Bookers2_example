f55a28920bc3083e132efca368a3e9edbae52a3a
kensuke1003 <kensukeseven1003@gmail.com>
2021-02-21 12:26:40 +0000

    middle

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,34 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+    @books = Book.all
+    @users = User.all
+    @user = current_user
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image,:introduction)
+  end
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9b3b3eebbd1db5451f16aad8ecb6a5fdfa3996e3d2a598896058baed891330be1f48eb962774618c02a062d24ac7117f889fc2efee46d328f9850aa697b5f665'
