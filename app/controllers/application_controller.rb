76fbbca79fdbc77d6d60cd47f01eaa8fc4a490fc
kensuke1003 <kensukeseven1003@gmail.com>
2021-03-19 11:25:39 +0000

    319

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,13 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
   end
+  def after_sign_up_path_for(resource)
+    user_path(current_user.id)
+  end
+  
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
 end
